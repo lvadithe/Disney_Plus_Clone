770e0f5451e5278218463d3d4f6da07778736da1
vadith gomez <legrone@MacBook-Pro.local>
2022-02-21 14:43:11 -0500

    first

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,7 +6,12 @@ function Login() {
         <div className="container__login">
             <div className="content__login">
                 <img src="/images/login__logo.png" alt="" className="content__logo_one" />
-                
+                <a href="" className="sign__up">GET ALL THERE</a>
+                <p className="description__component">
+                    Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+
+                    subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                </p>
+                <img src="/images/cta-logo-two.png" alt="" className="logo__two" />
             </div>
         </div>
     )
