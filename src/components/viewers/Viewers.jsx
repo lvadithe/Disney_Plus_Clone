bfc7ccd67a41afcd6a1808b6ffd5914d508ea4a8
vadith gomez <legrone@MacBook-Pro.local>
2022-02-21 19:20:26 -0500

    first

--- src/components/viewers/Viewers.jsx
+++ src/components/viewers/Viewers.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import './viewers.css';
 
 function Viewers() {
-  return (
-    <div className="component__viewers">
-        <div className="wrap__img__viewer">
-            <img className='img__viewer' src='/images/viewers-disney.png' />
+    return (
+        <div className="component__viewers">
+            <div className="wrap__img__viewer">
+                <img className='img__viewer' src='/images/viewers-disney.png' />
+                {/* <video src="/video/1564674844-disney.mp4"></video> */}
+            </div>
+            <div className="wrap__img__viewer">
+                <img className='img__viewer' src='/images/viewers-pixar.png' />
+                {/* <video src="/video/1564676714-pixar.mp4"></video> */}
+            </div>
+            <div className="wrap__img__viewer">
+                <img className='img__viewer' src='/images/viewers-marvel.png' />
+               {/*  <video src="/video/1564676115-marvel.mp4"></video> */}
+            </div>
+            <div className="wrap__img__viewer">
+                <img className='img__viewer' src='/images/viewers-starwars.png' />
+                {/* <video src="/video/1608229455-star-wars.mp4"></video> */}
+            </div>
+            <div className="wrap__img__viewer">
+                <img className='img__viewer' src='/images/viewers-national.png' />
+                {/* <video className='video_l' autoPlay loop={true} playsInline={true}>
+                    <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
+                </video> */}
+            </div>
         </div>
-        <div className="wrap__img__viewer">
-            <img className='img__viewer' src='/images/viewers-pixar.png' />
-        </div>
-        <div className="wrap__img__viewer">
-            <img className='img__viewer' src='/images/viewers-marvel.png' />
-        </div>
-        <div className="wrap__img__viewer">
-            <img className='img__viewer' src='/images/viewers-starwars.png' />
-        </div>
-        <div className="wrap__img__viewer">
-            <img className='img__viewer' src='/images/viewers-national.png' />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Viewers
